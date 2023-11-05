ec80c1f04ab3adf98ea9644a48fb1ba84d32a4c5
BarbaraPerSan <barbarapersan09@gmail.com>
2023-11-05 13:58:04 +0100

    TFG

---
+++ TFG/templatemo_559_zay_shop/assets/BD/Temas.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('1','Novela Romántica');
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('2','Fantasía');
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('3','Aventuras');
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('4','Ciencia Ficción');
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('5','Novela Histórica');
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('6','Novela Literaria');
+INSERT INTO temas (idTemas, Nombre)
+VALUES ('7','Ficción Juvenil');
