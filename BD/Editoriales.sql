c26764d1a778566b37660f8966b01a7639ac9bbe
Barbarapersan09 <149799609+Barbarapersan09@users.noreply.github.com>
2023-11-05 13:25:12 +0100

    Base de datos Mysql

---
+++ BD/Editoriales.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO editorial (idEditorial, Nombre, Direccion, Telefono, web)
+VALUES ('1','b de bolsillo', 'Penguin Libros',0,0);
+INSERT INTO Libros VALUES ('2', 'Montena', 'Penguin Libros',0,0)
---
+++ BD/Libros.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO Libros (idLibros, ISBN, Titulo, Tema, Páginas, Formato, Idioma, Fecha_publicación, Precio, Descripcion, Editorial_idEditorial)
+VALUES ('1',9788413145211, 'MENTIRAS Y SECRETOS (PERFECTOS MENTIROSOS 1)', 'Novela Romántica', 336, 'Tapa blanda', 'Castellano', '2025-10-16', 10.40, 'Una historia llena de mentiras y secretos, atracción y odio en la que nadie parece decir la verdad. ¿Hay algo más adictivo que desenmascarar a los Perfectos mentirosos?', 1);
